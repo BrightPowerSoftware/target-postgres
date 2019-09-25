49801dff9ca9d101cc14013c46c6f09d19f9f658
John Genere <johncgenere@gmail.com>
2019-09-25 10:15:48 -0400

    Removed unimplemented method

--- setup.py
+++ setup.py
@@ -8,9 +8,6 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-
-requirements = parse_requirements('requirements.txt')
-
 setup(
     name='singer-target-postgres',
     url='https://github.com/datamill-co/target-postgres',
